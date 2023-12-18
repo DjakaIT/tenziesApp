c091ba903e54a2533aeebed38e97ae7d08e2462f
DannyCroDev <danielrajic26@outlook.com>
2023-12-18 15:10:53 +0100

    added react-confetti library, implemented timed usage of it

--- src/App.jsx
+++ src/App.jsx
@@ -76,6 +76,7 @@ const diceElements = diceNum.map(diceNumber => {
 
   return(
     <main>
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
